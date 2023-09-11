1aaa9dac25fe5e911a2e47b16af4a209834d7c73
PietroFiori91 <pederflowers@hotmail.it>
2023-09-11 12:29:43 +0200

    function_exists

--- function.php
+++ function.php
@@ -1,12 +1,14 @@
 <?php
-function generatePassword($length) {
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
-    $password = '';
-    $charLength = strlen($characters);
+if (!function_exists('generatePassword')) {
+    function generatePassword($length) {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+        $password = '';
+        $charLength = strlen($characters);
 
-    for ($i = 0; $i < $length; $i++) {
-        $password .= $characters[rand(0, $charLength - 1)];
+        for ($i = 0; $i < $length; $i++) {
+            $password .= $characters[rand(0, $charLength - 1)];
+        }
+
+        return $password;
     }
-
-    return $password;
 }
--- index.php
+++ index.php
@@ -13,25 +13,25 @@
     </form>
 
     <?php
-    include 'function.php';
+    include './function.php';
 
     if (isset($_GET['length'])) {
         $password = generatePassword($_GET['length']);
         echo "<p>La tua password sicura è: $password</p>";
     }
 
-    function generatePassword($length)
-    {
-        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
-        $password = '';
-        $charLength = strlen($characters);
+    // function generatePassword($length)
+    // {
+    //     $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+    //     $password = '';
+    //     $charLength = strlen($characters);
 
-        for ($i = 0; $i < $length; $i++) {
-            $password .= $characters[rand(0, $charLength - 1)];
-        }
+    //     for ($i = 0; $i < $length; $i++) {
+    //         $password .= $characters[rand(0, $charLength - 1)];
+    //     }
 
-        return $password;
-    }
+    //     return $password;
+    // }
     ?>
 </body>
 
